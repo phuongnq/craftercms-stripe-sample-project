d902613b1553db1f412696991d2e14923b5c6806
Phil Nguyen <nquangphuong@gmail.com>
2023-03-06 17:22:22 +0700

    Update

--- static-assets/plugins/org/craftercms/plugin/stripe/js/script.js
+++ static-assets/plugins/org/craftercms/plugin/stripe/js/script.js
@@ -1,9 +1,11 @@
 /* Fetch prices and update the form */
 fetch("/api/plugins/org/craftercms/plugin/stripe/config.json")
   .then(r => r.json())
-  .then(({basicPrice, proPrice}) => {
+  .then(({ basicPrice, proPrice, enterprisePrice }) => {
     const basicPriceInput = document.querySelector('#basicPrice');
     basicPriceInput.value = basicPrice;
     const proPriceInput = document.querySelector('#proPrice');
     proPriceInput.value = proPrice;
+    const enterprisePriceInput = document.querySelector('#enterprisePrice');
+    enterprisePriceInput.value = enterprisePrice;
   })
