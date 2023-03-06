7f5a934e99b58c90ea70f94900ed3c3cbafc6155
Phil Nguyen <nquangphuong@gmail.com>
2023-03-06 15:51:32 +0700

    test

--- scripts/rest/plugins/org/craftercms/plugin/stripe/checkout-session.get.groovy
+++ scripts/rest/plugins/org/craftercms/plugin/stripe/checkout-session.get.groovy
@@ -1,8 +1,10 @@
 @Grapes([
-    @Grab(group='com.stripe', module='stripe-java', version='22.10.0', initClass=false)
+    @Grab(group='com.stripe', module='stripe-java', version='22.10.0', initClass=false),
+    @Grab(group='com.google.code.gson', module='gson', version='2.10', initClass=false)
 ])
 
 import com.stripe.model.checkout.Session
+import com.google.gson.Gson
 
 def sessionId = params.sessionId
 
@@ -10,4 +12,7 @@ if (!sessionId) {
     return [:]
 }
 
-return Session.retrieve(sessionId)
+Session session = Session.retrieve(sessionId)
+Gson gson = new Gson()
+
+return gson.toJson(session))
