c362262008b5ecd6ff748804bda6a3724c21b8c3
Phil Nguyen <nquangphuong@gmail.com>
2023-03-06 14:28:42 +0700

    Add config rest api

--- scripts/rest/plugins/org/craftercms/plugin/stripe/config.get.groovy
+++ scripts/rest/plugins/org/craftercms/plugin/stripe/config.get.groovy
@@ -1,7 +1,7 @@
 def result = [:]
 
 result.publishableKey = siteConfig.getString('stripe.publishableKey')
-result.basicPrice = siteConfig.getString('basicPrice')
-result.proPrice = siteConfig.getString('proPrice')
+result.basicPrice = siteConfig.getString('stripe.basicPrice')
+result.proPrice = siteConfig.getString('stripe.proPrice')
 
 return result
