3721368fa65643835b7b137ca851281974de8e7c
Phil Nguyen <nquangphuong@gmail.com>
2023-03-06 17:15:24 +0700

    Update

--- scripts/rest/plugins/org/craftercms/plugin/stripe/config.get.groovy
+++ scripts/rest/plugins/org/craftercms/plugin/stripe/config.get.groovy
@@ -3,6 +3,6 @@ def result = [:]
 result.publishableKey = siteConfig.getString('stripe.publishableKey')
 result.basicPrice = siteConfig.getString('stripe.basicPriceId')
 result.proPrice = siteConfig.getString('stripe.proPriceId')
-result.enterprisePrice = siteConfig.getString('stripe.enterprisePrice')
+result.enterprisePrice = siteConfig.getString('stripe.enterprisePriceId')
 
 return result
